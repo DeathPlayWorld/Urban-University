893a819b53d1a3a86a736642b736e94793c8741b
DeathPlayWorld <43905820+DeathPlayWorld@users.noreply.github.com>
2024-11-05 19:37:06 +0800

    Add files via upload

--- Практика GIT. Базовые структуры данных/2.1/module_1_3.py
+++ Практика GIT. Базовые структуры данных/2.1/module_1_3.py
@@ -12,7 +12,11 @@ _Age = int(_Age) + int(_NewAge)
 print("Congratulations! Now your age is", str(_Age) + "!")
 print("Are you a student?")
 print("'1' - yes      '0' - no")
-_Is_Student = bool(input())
+_Is_Student = int(input())
+if (_Is_Student == 0):
+    _Is_Student = bool(False)
+else:
+    _Is_Student = bool(True)
 print("So......")
 print("Your name is", _Name+",", "you are", _Age, "years old.")
 print("Checking to see if you are a student:", _Is_Student)
