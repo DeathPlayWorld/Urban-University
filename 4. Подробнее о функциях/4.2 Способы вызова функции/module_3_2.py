4254426bd76bb501ea8ad04151b9125a3f11f275
DeathPlayWorld <43905820+DeathPlayWorld@users.noreply.github.com>
2024-11-23 19:45:04 +0800

    Add files via upload

---
+++ 4. Подробнее о функциях/4.2 Способы вызова функции/module_3_2.py
@@ -0,0 +1,36 @@
+
+def validating_domain(domain):
+    while domain[0] != ".": domain = domain[1:-1] + domain[-1]
+    return domain
+
+def get_domain(address:str):
+    domain = address[-4:-1] + address[-1]
+    return validating_domain(domain)
+
+def valid_mail(sender, recipient):
+    #print(get_domain(sender), "-", get_domain(recipient))
+    if ((get_domain(sender) != ".ru" and get_domain(sender) != ".com" and get_domain(sender) != ".net") or
+        (get_domain(recipient) != ".ru" and get_domain(recipient) != ".com" and get_domain(recipient) != ".net") or
+        ("@" not in sender or "@" not in recipient)):
+        return "Domain_Error"
+    elif sender == recipient:
+        return "Self_Message"
+    else: return "Valid_Message"
+
+def send_email(message, recipient, *,sender = "university.help@gmail.com"):
+    match valid_mail(sender, recipient):
+        case "Domain_Error":
+            print("\nНевозможно отправить письмо с адреса", sender, "на адрес", recipient)
+        case "Self_Message":
+            print("\nНельзя отправить письмо самому себе!")
+        case "Valid_Message":
+            if sender != "university.help@gmail.com":
+                print("\nНЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса", sender, "на адрес", recipient)
+            else:
+                print("\nПисьмо успешно отправлено с адреса", sender, "на адрес", recipient)
+
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
