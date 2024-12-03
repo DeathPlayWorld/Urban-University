33761fcf4126bc8ca012f93bd19a2e9ded2604bb
DeathPlayWorld <43905820+DeathPlayWorld@users.noreply.github.com>
2024-12-03 12:01:20 +0800

    Delete Практика GIT. Базовые структуры данных directory

--- Практика GIT. Базовые структуры данных/2.2 Переменные/homework_2.py
+++
@@ -1,7 +0,0 @@
-_completed_works = 12
-_total_hours = 1.5
-_course_name = "Python"
-_work_time = _total_hours / _completed_works
-
-print("Course:", _course_name + ",", "total hours worked:", str(_total_hours) + ",",
-      "average time of work", str(_work_time) + ".")
