07b34d89ee99ba04f4e74363cedb091223e69d91
DeathPlayWorld <43905820+DeathPlayWorld@users.noreply.github.com>
2024-12-10 10:22:40 +0800

    Add files via upload

---
+++ 6. Пространство имен и области видимости/6.1 Пространство имен/main.py
@@ -0,0 +1,7 @@
+def test_function():
+    def inner_function():
+        print("Inner function in the field of test_function")
+    inner_function()
+
+test_function()
+#inner_function() will be error. Very similar to encapsulation in the OOP.
